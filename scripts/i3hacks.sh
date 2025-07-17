585836eb48e2825156e223612c90c36bb0cc308b
dragsbruh <dragsbruh@protonmail.com>
2025-07-18 00:02:05 +0530

    (feat) i3 + picom!!!

---
+++ scripts/i3hacks.sh
@@ -0,0 +1,11 @@
+# parts blatantly stolen from https://github.com/maxwell-bland/i3-natural-scrolling-and-tap/blob/master/inverse-scroll.sh
+# dont tell anyone, tho :3
+
+exec xinput set-prop "MSFT0001:01 04F3:31BE Touchpad" "libinput Tapping Enabled" 1
+
+id=`xinput list | grep -i "Touchpad" | cut -d'=' -f2 | cut -d'[' -f1`
+natural_scrolling_id=`xinput list-props $id | \
+                      grep -i "Natural Scrolling Enabled (" \
+                      | cut -d'(' -f2 | cut -d')' -f1`
+
+xinput --set-prop $id $natural_scrolling_id 1
