2f85c04956d519da778090dc64b17a85c2b8a40f
dragsbruh <dragsbruh@protonmail.com>
2025-07-29 19:04:43 +0530

    fix: stupid

--- scripts/i3hacks.sh
+++ scripts/i3hacks.sh
@@ -1,7 +1,7 @@
 # parts blatantly stolen from https://github.com/maxwell-bland/i3-natural-scrolling-and-tap/blob/master/inverse-scroll.sh
 # dont tell anyone, tho :3
 
-exec xinput set-prop "MSFT0001:01 04F3:31BE Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "MSFT0001:01 04F3:31BE Touchpad" "libinput Tapping Enabled" 1
 
 id=`xinput list | grep -i "Touchpad" | cut -d'=' -f2 | cut -d'[' -f1`
 natural_scrolling_id=`xinput list-props $id | \
